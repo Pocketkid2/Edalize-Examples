4b81289c553a04a117303986d1dd834818119423
Adam Taylor <afranklint16@gmail.com>
2022-06-15 16:00:23 -0600

    Added Vivado edalize file to Debounce, still works

---
+++ debounce/debounce_vivado.py
@@ -0,0 +1,24 @@
+from edalize import *
+import os
+
+work_root = 'build'
+files = [
+    {'name' : os.path.relpath('debounce.sv', work_root), 'file_type' : 'systemVerilogSource'},
+    {'name' : os.path.relpath('debounce_top.sv', work_root), 'file_type' : 'systemVerilogSource'},
+    {'name' : os.path.relpath('SevenSegmentControl.sv', work_root), 'file_type' : 'systemVerilogSource'},
+    {'name' : os.path.relpath('basys3.xdc', work_root), 'file_type' : 'xdc'}
+]
+
+tool = 'vivado'
+edam = {
+    'files' : files,
+    'name' : 'Debounce',
+    'parameters' : {},
+    'toplevel' : 'debounce_top',
+    'tool_options' : {'vivado' : {'part' : 'xc7a35tcpg236-1'}}
+
+}
+
+backend = get_edatool(tool)(edam = edam, work_root = work_root)
+os.makedirs(work_root)
+backend.configure()
