a731cb0635951c6eb3bd2e33d171620b292a3612
Adam Taylor <afranklint16@gmail.com>
2022-06-27 15:04:22 -0600

    Split F4pga edalize script into VPR and NextPNR options (NextPNR requires .bba/.bin file from NextPNR-Xilinx)

--- blinky/blinky_nextpnr.py
+++ blinky/blinky_nextpnr.py
@@ -1,10 +1,11 @@
 from edalize import *
 import os
 
-work_root = 'build'
+work_root = 'build_nextpnr'
 files = [
     {'name' : os.path.relpath('blinky.v', work_root), 'file_type' : 'verilogSource'},
-    {'name' : os.path.relpath('blinky.xdc', work_root), 'file_type' : 'xdc'}
+    {'name' : os.path.relpath('blinky_nextpnr.xdc', work_root), 'file_type' : 'xdc'},
+    {'name' : os.path.relpath('xc7a35t.bin', work_root), 'file_type' : 'bin'}
 ]
 tool = 'f4pga'
 edam = {
@@ -17,7 +18,8 @@ edam = {
             'arch': 'xilinx',
             'device_type': 'artix7',
             'device_name': 'xc7a50t_test',
-            'part' : 'xc7a35tcpg236-1'
+            'part' : 'xc7a35tcpg236-1',
+            'pnr' : 'nextpnr'
         }
     }
 }
@@ -25,4 +27,5 @@ edam = {
 backend = get_edatool(tool)(edam = edam, work_root = work_root)
 os.makedirs(work_root)
 backend.configure()
-#backend.build()
+backend.build()
+backend.run()
