0d741b63ae586ea450dc95e9cc1ce3e0ae6650c5
Adam Taylor <afranklint16@gmail.com>
2022-07-01 12:59:56 -0600

    Merged constraints for receiver project

--- rx/rx_nextpnr.py
+++ rx/rx_nextpnr.py
@@ -6,7 +6,7 @@ files = [
     {'name' : os.path.relpath('rx.sv', work_root), 'file_type' : 'systemVerilogSource'},
     {'name' : os.path.relpath('rx_top.sv', work_root), 'file_type' : 'systemVerilogSource'},
     {'name' : os.path.relpath('SevenSegmentControl.sv', work_root), 'file_type' : 'systemVerilogSource'},
-    {'name' : os.path.relpath('basys3_nextpnr.xdc', work_root), 'file_type' : 'xdc'},
+    {'name' : os.path.relpath('basys3.xdc', work_root), 'file_type' : 'xdc'},
     {'name' : os.path.relpath('xc7a35t.bin', work_root), 'file_type' : 'bin'}
 ]
 
