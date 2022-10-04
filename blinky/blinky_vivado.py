e667aa6f5d146b078cbb4ac377b1bfe8db52fbaa
Adam Taylor <afranklint16@gmail.com>
2022-10-04 17:04:53 -0600

    Cleanup extra scripts for now and fix the blinky vivado script

--- blinky/blinky_vivado.py
+++ blinky/blinky_vivado.py
@@ -19,5 +19,5 @@ edam = {
 backend = get_edatool(tool)(edam = edam, work_root = work_root)
 os.makedirs(work_root)
 backend.configure()
-#backend.build()
-#backend.run()
+backend.build()
+backend.run()
